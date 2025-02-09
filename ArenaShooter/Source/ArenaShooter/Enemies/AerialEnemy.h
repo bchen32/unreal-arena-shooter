19a9496b200895c2006703ed21f04b6029ec8c04
ThatWugg <struberryjam@gmail.com>
2025-02-09 15:20:56 -0600

    added a really basic aerial enemy (its just a groudn enemy with hitbox in air) that acts just like the shooter enemy

---
+++ ArenaShooter/Source/ArenaShooter/Enemies/AerialEnemy.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemies/ShooterEnemy.h"
+#include "AerialEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENASHOOTER_API AAerialEnemy : public AShooterEnemy
+{
+	GENERATED_BODY()
+};
